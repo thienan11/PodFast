a85cc7d1cb5e3b7993b2eb8829fefe36cb20afff
Thien An Tran <fxthienan@gmail.com>
2025-05-09 12:53:08 -0700

    feat: add avatar and sonner components; fix styling in signin and signout page

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -5,15 +5,17 @@ export default function SignInPage() {
   return (
     <div className="w-full flex mt-20 justify-center">
       <section className="flex flex-col w-[400px]">
-        <h1 className="text-3xl w-full text-center font-bold mb-6">Sign in</h1>
+        <h1 className="text-3xl w-full text-center font-bold mb-6 text-primary-foreground">
+          Sign in
+        </h1>
         <SignIn />
-        <div className="mt-6 flex items-center px-5 sm:px-0">
+        <div className="mt-6 flex items-center px-5 sm:px-0 text-primary-foreground">
           <h1>{`Don't have an account?`}</h1>
           <Link className="font-bold ml-2 hover:underline" href="/signup">
             Sign Up
           </Link>
         </div>
-        <div className="mt-3 flex items-center px-5 sm:px-0">
+        <div className="mt-3 flex items-center px-5 sm:px-0 text-primary-foreground">
           <Link className="font-bold hover:underline" href="/forgot-password">
             <h1>{`Forgot your password?`}</h1>
           </Link>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -5,11 +5,11 @@ export default async function SignUpPage() {
   return (
     <div className="w-full flex mt-20 justify-center">
       <section className="flex flex-col w-[400px]">
-        <h1 className="text-3xl w-full text-center font-bold mb-6">Sign Up</h1>
+        <h1 className="text-3xl w-full text-center font-bold mb-6 text-primary-foreground">Sign Up</h1>
         <SignUp />
-        <div className="mt-6 flex items-center px-5 sm:px-0">
+        <div className="mt-6 flex items-center px-5 sm:px-0 text-primary-foreground">
           <h1>Already have an account?</h1>
-          <Link className="font-bold ml-2 hover:underline" href="/signin">
+          <Link className="font-bold ml-2 hover:underline text-primary-foreground" href="/signin">
             Sign In
           </Link>
         </div>
